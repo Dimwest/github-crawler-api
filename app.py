e83168e313b46c1633f7e44bcd852278a675cc6e
Dimwest <dimwestdev@gmail.com>
2019-08-28 23:59:50 +0200

    Improve app.py

--- app.py
+++ app.py
@@ -1,39 +1,54 @@
-from chalice import Chalice
+from chalice import Chalice, Response
 from chalicelib.crawler import get_monthly_new_contributors
-from github import GithubException, RateLimitExceededException, BadCredentialsException
+from github import GithubException, \
+    RateLimitExceededException, BadCredentialsException
 
-app = Chalice(app_name='github-crawler')
+app = Chalice(app_name="github-crawler")
 
 
-@app.route('monthly_new_contributors/<username>')
+@app.route("/monthly_new_contributors/{username}", methods=["GET"])
 def monthly_new_contributors_per_user(username: str):
 
     try:
         data = get_monthly_new_contributors(username)
-        print(data)
-        return data
+        return Response(
+            status_code=200,
+            body={
+                "data": data,
+                "message": f"Successfully retrieved new contributors "
+                           f"statistics for user {username}",
+            },
+        )
+
     except RateLimitExceededException:
-        return {
-            'status_code': 429,
-            'message': f'Rate Limit Exceeded Error: Github API hourly limit has been exceeded'
-        }
+        return Response(
+            status_code=429,
+            body={
+                "data": {},
+                "message": f"Rate Limit Exceeded Error: Github API "
+                           f"hourly limit has been exceeded",
+            },
+        )
     except BadCredentialsException:
-        return {
-            'status_code': 401,
-            'message': f'Bad Credentials Error: Invalid Github authentication token'
-        }
-    except GithubException as e:
-        return {
-            'status_code': 500,
-            'message': f'Github API Error: {type(e)} -> {e.args}'
-        }
-    except Exception as e:
-        return {
-            'status_code': 500,
-            'message': f'Internal server Error: {type(e)} -> {e.args}'
-        }
-
-
-# @app.route('monthly_new_contributors/<username>/<repo>')
-# def monthly_new_contributors_per_repo(username: str, repo: str):
-#     return get_monthly_new_contributors(username)
+        return Response(
+            status_code=401,
+            body={
+                "data": {},
+                "message": f"Bad Credentials Error: Invalid Github "
+                           f"authentication token",
+            },
+        )
+    except GithubException:
+        return Response(
+            status_code=500,
+            body={
+                "data": {},
+                "message": f"Github API Error: could not retrieve "
+                           f"data for user {username}",
+            },
+        )
+    except Exception:
+        return Response(
+            status_code=500, body={"data": {},
+                                   "message": f"Internal Server Error"}
+        )
